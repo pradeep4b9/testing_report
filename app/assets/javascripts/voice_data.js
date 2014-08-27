4c5f7773e3353d6690b238f249615fabdc0c2ca4
kamleshl <kamlesh.l@comakeit.com>
2014-08-27 11:18:46 +0530

    voice script error resolved

--- app/assets/javascripts/voice_data.js
+++ app/assets/javascripts/voice_data.js
@@ -1,19 +1,25 @@
-$('#CamButton').click(function() {
-  $(".error").hide("slow").remove();  
-  $('#CamPicker').trigger('click');
-  return false;
-});
+$(document).ready(function(){
 
-$('#CamPicker').on('change', function(e) {
-   var file = e.currentTarget.files[0];
 
-   if(file.type.search("audio")=="-1"){
-    $("<div class='error'>Only audio format is allowed</div>").insertBefore(".CamPicker");
+
+  $('#CamButton').click(function() {
+    $(".error").hide("slow").remove();  
+    $('#CamPicker').trigger('click');
     return false;
-   }else{
-    $(".voice-loader").show();
-    $("#formSubmit").attr("action","/card_scans/voice")
-    document.forms["formSubmit"].submit();
-   
-   }
-});          
+  });
+
+  $('#CamPicker').on('change', function(e) {
+     var file = e.currentTarget.files[0];
+
+     if(file.type.search("audio")=="-1"){
+      $("<div class='error'>Only audio format is allowed</div>").insertBefore(".CamPicker");
+      return false;
+     }else{
+      $(".voice-loader").show();
+      $("#formSubmit").attr("action","/card_scans/voice")
+      document.forms["formSubmit"].submit();
+     
+     }
+  });          
+
+})
