21e7cac57e51ccb7eb24f823a5995ca9e46d05b7
Shyam <shyammohankanojia@gmail.com>
2014-10-01 17:40:45 +0530

    bug fix

--- app/models/card_scan.rb
+++ app/models/card_scan.rb
@@ -7,7 +7,7 @@ class CardScan
 
   field :first_name, type: String
   field :middle_name, type: String
-  field :last_name. type: String
+  field :last_name, type: String
 
   field :date_of_birth, type: Date
   field :sex, type: String
