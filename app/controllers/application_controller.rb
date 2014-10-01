3ab76039edd4301f9e46491733855407069d7364
Shyam <shyammohankanojia@gmail.com>
2014-10-01 17:03:45 +0530

    mobile_location removed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,30 +5,30 @@ class ApplicationController < ActionController::Base
   # before_filter :mobile_location
 
 
-  def mobile_location
-  	dev_ip = "199.193.252.231" # USA
-  	# dev_ip = "207.179.146.70" # Canada
-  	# dev_ip = "182.73.163.98" #Asia
-  	# dev_ip = "5.11.143.22" #Europe
-  	# dev_ip = "202.138.79.255" # Australia
-  	# dev_ip = "201.221.132.22" # South America
+  # def mobile_location
+  # 	dev_ip = "199.193.252.231" # USA
+  # 	# dev_ip = "207.179.146.70" # Canada
+  # 	# dev_ip = "182.73.163.98" #Asia
+  # 	# dev_ip = "5.11.143.22" #Europe
+  # 	# dev_ip = "202.138.79.255" # Australia
+  # 	# dev_ip = "201.221.132.22" # South America
 
-  	ip = Rails.env == "production" ? request.remote_ip : dev_ip
-  	data = Geocoder.search ip
+  # 	ip = Rails.env == "production" ? request.remote_ip : dev_ip
+  # 	data = Geocoder.search ip
 
-  	continent = "Sorry, Not able to find your location."
-  	country = data[0].data["country_name"] if data[0]
-  	if country
-	  	if country == "Reserved"
-	  		continent = "Sorry, Not able to find your location."
-	  	else
-		  	continent = Country.find_country_by_name(country).continent
-		  	continent = "Canada" if country == "Canada"
-		  	continent = "USA" if country == "United States"
-		  end
-	  end
-	  session[:continent] = continent
-  end
+  # 	continent = "Sorry, Not able to find your location."
+  # 	country = data[0].data["country_name"] if data[0]
+  # 	if country
+	 #  	if country == "Reserved"
+	 #  		continent = "Sorry, Not able to find your location."
+	 #  	else
+		#   	continent = Country.find_country_by_name(country).continent
+		#   	continent = "Canada" if country == "Canada"
+		#   	continent = "USA" if country == "United States"
+		#   end
+	 #  end
+	 #  session[:continent] = continent
+  # end
 
   def after_sign_in_path_for(resource_or_scope)
     if current_user.mobile_number.blank?
@@ -39,7 +39,7 @@ class ApplicationController < ActionController::Base
       card_scans_path
     else
       dashboard_index_path
-    end      
+    end
   end
 
 end
