2a080a76c15c56a32e8ad56cbb2a62f484d1c757
Shyam <shyammohankanojia@gmail.com>
2014-10-06 15:18:26 +0530

    sidekiq fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,22 +35,22 @@ set :puma_workers, 0
 set :puma_init_active_record, true
 set :puma_preload_app, true
 
-# set :sidekiq_config, -> { File.join(shared_path, 'config', 'sidekiq.yml') }
+set :sidekiq_config, -> { File.join(shared_path, 'config', 'sidekiq.yml') }
 # :sidekiq_default_hooks =>  true
-:sidekiq_pid =>  File.join(shared_path, 'tmp', 'pids', 'sidekiq.pid')
-:sidekiq_env =>  fetch(:rack_env, fetch(:rails_env, fetch(:stage)))
-:sidekiq_log =>  File.join(shared_path, 'log', 'sidekiq.log')
-:sidekiq_options =>  nil
-:sidekiq_require => nil
-:sidekiq_tag => nil
-:sidekiq_config => nil
-:sidekiq_queue => nil
-:sidekiq_timeout =>  10
-:sidekiq_role =>  :app
-:sidekiq_processes =>  1
-:sidekiq_concurrency => nil
-:sidekiq_cmd => "#{fetch(:bundle_cmd, "bundle")} exec sidekiq"  # Only for capistrano2.5
-:sidekiqctl_cmd => "#{fetch(:bundle_cmd, "bundle")} exec sidekiqctl" # Only for capistrano2.5
+# :sidekiq_pid =>  File.join(shared_path, 'tmp', 'pids', 'sidekiq.pid')
+# :sidekiq_env =>  fetch(:rack_env, fetch(:rails_env, fetch(:stage)))
+# :sidekiq_log =>  File.join(shared_path, 'log', 'sidekiq.log')
+# :sidekiq_options =>  nil
+# :sidekiq_require => nil
+# :sidekiq_tag => nil
+# :sidekiq_config => nil
+# :sidekiq_queue => nil
+# :sidekiq_timeout =>  10
+# :sidekiq_role =>  :app
+# :sidekiq_processes =>  1
+# :sidekiq_concurrency => nil
+# :sidekiq_cmd => "#{fetch(:bundle_cmd, "bundle")} exec sidekiq"  # Only for capistrano2.5
+# :sidekiqctl_cmd => "#{fetch(:bundle_cmd, "bundle")} exec sidekiqctl" # Only for capistrano2.5
 
 
 namespace :deploy do
